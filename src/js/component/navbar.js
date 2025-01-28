f7e90c2ba693ab655bbb819f957c4ac463c88b8b
Irene Batlle Mola <ibatllemola@gmail.com>
2025-01-28 20:44:27 +0000

    home terminado, navbar casi

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -2,13 +2,15 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../../styles/index.css";
 
+
 export const Navbar = () => {
 	return (
-	  <nav className="navbar mb-3">
-		<div className="navbar-content">
-		  <img className="star-wars-logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Star_Wars_Logo..png/640px-Star_Wars_Logo..png" alt="Star Wars Logo" />
-		</div>
-	  </nav>
+		<nav className="navbar mb-3">
+			<div className="navbar-content">
+				<Link to="/">
+					<img className="star-wars-logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Star_Wars_Logo..png/640px-Star_Wars_Logo..png" alt="Star Wars Logo" />
+				</Link>
+			</div>
+		</nav>
 	);
-  };
-  
+};
